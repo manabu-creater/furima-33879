d2560401d0f885502ef72844e5d47729735e394b
manabu-creater <69703387+manabu-creater@users.noreply.github.com>
2021-03-02 19:52:45 +0900

    修正版３

--- app/javascript/items.js
+++ app/javascript/items.js
@@ -2,15 +2,15 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById('item-price');
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue)
+    //console.log(inputValue)
 
   const addTaxDom = document.getElementById("add-tax-price");
   addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-    console.log(addTaxDom);
+    //console.log(addTaxDom);
 
   const profitFigure = document.getElementById("profit");
     const valueResult = inputValue * 0.1
     profitFigure.innerHTML = (Math.floor(inputValue - valueResult));
-      console.log(profitFigure);
+      //console.log(profitFigure);
   })
 })
